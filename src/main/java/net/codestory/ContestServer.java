c23d48f5e4cb1df7fc40b4af008aa66c94386d27
David Gageot <david@gageot.net>
2012-12-21 11:23:56 +0100

    Remove some duplication

--- src/main/java/net/codestory/ContestServer.java
+++ src/main/java/net/codestory/ContestServer.java
@@ -1,13 +1,20 @@
 package net.codestory;
 
+import com.google.common.base.Charsets;
+import com.google.common.base.Throwables;
 import com.google.common.io.Files;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import net.codestory.templating.ContentWithVariables;
+import net.codestory.templating.Layout;
+import net.codestory.templating.Template;
+import net.codestory.templating.YamlFrontMatter;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Map;
 
 public class ContestServer implements HttpHandler {
   private HttpServer server;
@@ -18,13 +25,14 @@ public class ContestServer implements HttpHandler {
 
     System.out.println(path);
 
-    byte[] response;
+    String body;
     if (path.equals("/faq")) {
-      response = Files.toByteArray(new File("web/faq.html"));
+      body = templatize(read("faq.html"));
     } else {
-      response = Files.toByteArray(new File("web/index.html"));
+      body = templatize(read("index.html"));
     }
 
+    byte[] response = body.getBytes();
     exchange.sendResponseHeaders(200, response.length);
     exchange.getResponseBody().write(response);
     exchange.close();
@@ -38,5 +46,48 @@ public class ContestServer implements HttpHandler {
 
   public void stop() {
     server.stop(0);
+  }
+
+  private String templatize(String text) {
+    ContentWithVariables yaml = new YamlFrontMatter().parse(text);
+    String content = yaml.getContent();
+    Map<String, String> variables = yaml.getVariables();
+
+    String layout = variables.get("layout");
+    if (layout != null) {
+      content = new Layout(read(layout)).apply(content);
+    }
+
+    return new Template().apply(content, variables);
+  }
+
+  private String read(String path) {
+    return read(file(path));
+  }
+
+  private String read(File file) {
+    try {
+      return Files.toString(file, Charsets.UTF_8);
+    } catch (IOException e) {
+      throw Throwables.propagate(e);
+    }
+  }
+
+  private File file(String path) {
+    if (path.endsWith("/")) {
+      throw new IllegalStateException("Not found");
+    }
+
+    try {
+      File root = new File("web");
+      File file = new File(root, path);
+      if (!file.exists() || !file.getCanonicalPath().startsWith(root.getCanonicalPath())) {
+        throw new IllegalStateException("Not found");
+      }
+
+      return file;
+    } catch (IOException e) {
+      throw Throwables.propagate(e);
+    }
   }
 }
