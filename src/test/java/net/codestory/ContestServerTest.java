3cf60837e62a5be30c384a3336379d376a59f9f8
David Gageot <david@gageot.net>
2012-12-21 10:38:53 +0100

    Home page basic layout

--- src/test/java/net/codestory/ContestServerTest.java
+++ src/test/java/net/codestory/ContestServerTest.java
@@ -2,6 +2,7 @@ package net.codestory;
 
 import net.gageot.test.rules.ServiceRule;
 import net.sourceforge.jwebunit.junit.WebTester;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -9,12 +10,32 @@ public class ContestServerTest extends WebTester {
   @Rule
   public ServiceRule<ContestServer> server = ServiceRule.startWithRandomPort(ContestServer.class);
 
+  @Before
+  public void setUp() {
+    setBaseUrl("http://localhost:" + server.getPort());
+  }
+
   @Test
   public void should_display_welcome() {
-    setBaseUrl("http://localhost:" + server.getPort());
-
     beginAt("/");
 
-    assertTextPresent("Welcome to the 2013 Code-Story Context");
+    assertTextPresent("Welcome");
+    assertTextPresent("to our 2013 Contest");
+  }
+
+  @Test
+  public void should_display_links() {
+    beginAt("/");
+
+    assertLinkPresentWithExactText("Home");
+    assertLinkPresentWithExactText("Blog");
+    assertLinkPresentWithExactText("Contact");
+  }
+
+  @Test
+  public void should_sign_up() {
+    beginAt("/");
+
+    assertLinkPresentWithExactText("Sign up today");
   }
 }
