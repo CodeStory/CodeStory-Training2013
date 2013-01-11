43d910c74511859c3df17ee1567960ad909c427f
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2013-01-11 01:02:05 +0100

    ability to launch GraphGenerator from command line

--- src/main/java/codestory/GraphGenerator.java
+++ src/main/java/codestory/GraphGenerator.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.util.Map;
 
 import static java.io.File.separatorChar;
+import static java.lang.String.format;
+import static java.lang.System.exit;
 import static java.util.Collections.unmodifiableMap;
 
 public class GraphGenerator {
@@ -13,6 +15,17 @@ public class GraphGenerator {
         final Logins logins = new Logins(new File(directory, "logins"));
 
         return unmodifiableMap(logins.getScores(steps));
+    }
+
+    public static void main(String... args) {
+        if (args.length != 1) {
+            System.err.println("usage: java " + GraphGenerator.class.getCanonicalName() + " <directory>");
+            exit(1);
+        }
+
+        for (final Map.Entry<String, Integer> score : new GraphGenerator().getScores(new File(args[0])).entrySet()) {
+            System.out.println(format("%3d %s", score.getValue(), score.getKey()));
+        }
     }
 
 }
