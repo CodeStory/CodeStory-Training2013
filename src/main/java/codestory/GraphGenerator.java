bc66d3f4171e13acbb50966fbff3fae23f62d217
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2013-01-11 20:05:09 +0100

    scan git project and try to generate scores for each commit

--- src/main/java/codestory/GraphGenerator.java
+++ src/main/java/codestory/GraphGenerator.java
@@ -1,10 +1,16 @@
 package codestory;
 
-import java.io.File;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryBuilder;
+import org.eclipse.jgit.revwalk.RevCommit;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+import static java.io.File.separator;
 import static java.io.File.separatorChar;
 import static java.lang.String.format;
 import static java.lang.System.exit;
@@ -25,14 +31,37 @@ public class GraphGenerator {
         return unmodifiableMap(scores);
     }
 
+    public Set<String> commits(final File gitDir) {
+        final LinkedHashSet<String> commits = new LinkedHashSet<>();
+        final Repository repository;
+        try {
+            repository = new RepositoryBuilder().setGitDir(gitDir).findGitDir().setMustExist(true).build();
+            final Git git = new Git(repository);
+            for (RevCommit revCommit : git.log().call()) {
+                commits.add(revCommit.getId().name());
+            }
+        } catch (IOException | GitAPIException e) {
+            e.printStackTrace();
+        }
+        return commits;
+    }
+
     public static void main(String... args) {
         if (args.length != 1) {
             System.err.println("usage: java " + GraphGenerator.class.getCanonicalName() + " <directory>");
             exit(1);
         }
 
-        for (final Map.Entry<String, Integer> scoreByLogins : new GraphGenerator().getScores(new File(args[0])).entrySet()) {
-            System.out.println(format("%3d %s", scoreByLogins.getValue(), scoreByLogins.getKey()));
+        final File directory = new File(args[0]);
+        final GraphGenerator graphGenerator = new GraphGenerator();
+
+        System.out.println("cd " + directory.getAbsolutePath());
+        for (String commit : graphGenerator.commits(new File(args[0] + separator + ".git"))) {
+            System.out.println("git reset --hard " + commit);
+            for (final Map.Entry<String, Integer> scoreByLogins : graphGenerator.getScores(directory).entrySet()) {
+                System.out.println(format("%3d %s", scoreByLogins.getValue(), scoreByLogins.getKey()));
+            }
+            System.out.println();
         }
     }
 
